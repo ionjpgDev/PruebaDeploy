1dd8fa89b6cc88efca06eb028e630d8c9acb6bc1
Jhonatan Plata <platacruzjhonatan@gmail.com>
2026-05-20 15:50:54 -0400

    cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://ionjpgDev.github.io',
+  	base: '/PruebaDeploy',
 	integrations: [
 		starlight({
 			title: 'My Docs',
